b170ed3c87a9ddd812147ef846d4cc02c2111c56
plaffey <peter.laffey@gmail.com>
2016-06-16 22:14:49 -0500

    Refactor AddTodo to work with Redux

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -4,7 +4,7 @@ var moment = require("moment");
 
 
 import TodoList from "TodoList";
-var AddTodo = require("AddTodo");
+import AddTodo from "AddTodo";
 var TodoSearch = require("TodoSearch");
 var TodoAPI = require("TodoAPI");
 
